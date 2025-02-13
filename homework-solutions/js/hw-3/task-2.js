f044a8280b79fae89f00b8c45cf7de39cda7c377
IMac 27 <imac27@IMacs-iMac.local>
2025-02-13 12:41:59 +0100

    The homework has been done

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -4,3 +4,20 @@
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
+
+
+// Enter the number from 1 to 9
+let n = prompt;
+
+// Make the 'n' to the String and Concatenating
+
+let nn = n + n;
+let nnn = n + n + n;
+
+//Make the Number from the String and count
+
+let result = Number(n) + Number(nn) + Number(nnn);
+
+
+// Result
+console.log(`${n} + ${nn} + ${nnn} = ${result}`);
