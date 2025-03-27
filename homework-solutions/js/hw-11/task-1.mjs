e60903b64f0a166d333fc23973c473ed417452e1
AvramenkoDmitry <Avramenkodmytro1998@gmail.com>
2025-03-27 18:04:37 +0100

    Homework11

--- homework-solutions/js/hw-11/task-1.mjs
+++ homework-solutions/js/hw-11/task-1.mjs
@@ -1,9 +1,63 @@
 class Employee {
-  // Ваш код
+  constructor(firstName, lastName, profession, salary) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.profession = profession;
+    this._salary = salary;
+  }
+
+  get salary() {
+    return this._salary;
+  }
+
+  set salary(value) {
+    if (value > 0) {
+      this._salary = value;
+    } else {
+      throw new Error('Зарплата должна быть больше 0');
+    }
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
 
 class Company {
-  // Ваш код
+  constructor(title, phone, address) {
+    this._title = title;
+    this._phone = phone;
+    this._address = address;
+    this._employees = [];
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get phone() {
+    return this._phone;
+  }
+
+  get address() {
+    return this._address;
+  }
+
+  addEmployee(employee) {
+    if (employee instanceof Employee) {
+      this._employees.push(employee);
+    } else {
+      throw new Error('Переданный объект не является экземпляром класса Employee.');
+    }
+  }
+
+  getEmployees() {
+    return this._employees;
+  }
+
+  getInfo() {
+    return `Company: ${this._title}\nAddress: ${this._address}\nEmployees: ${this._employees.length}`;
+  }
 }
 
 export { Employee, Company };
